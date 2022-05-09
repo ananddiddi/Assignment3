1977a3414f1879339a13af4a3d4053aeb7175940
789254 <ananddiddi102@gmail.com>
2022-05-09 19:44:41 +0530

    added by diddi

---
+++ src/main/java/com/queue/QueueExample.java
@@ -0,0 +1,27 @@
+package com.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+
+	public static void main(String[] args){
+		
+		Queue<Integer> q= new LinkedList<>();
+		
+		for (int i = 0; i < 10; i++)q.add(i);
+		
+		System.out.println("Elements of queue "+ q);
+		
+		int removedele = q.remove();
+		System.out.println("removed element-" + removedele);
+
+		System.out.println(q);
+		
+		int head = q.peek();
+		System.out.println("head of queue-" + head);
+		int size = q.size();
+		
+		System.out.println("Size of queue-" + size);
+	}
+}
